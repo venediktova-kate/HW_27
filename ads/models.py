73d4c53a2e14ad30dba27ac46ea24119fe11a35a
venediktova-kate <venediktova.kat@gmail.com>
2023-05-08 00:18:05 +0700

    create two models

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Ad(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=200)
+    is_published = models.BooleanField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
