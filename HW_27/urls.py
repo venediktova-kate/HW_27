476c7fd0e7e774ab898e1f9705560b080e470d0a
venediktova-kate <venediktova.kat@gmail.com>
2023-05-08 01:06:41 +0700

    Add 'category' views

--- HW_27/urls.py
+++ HW_27/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from ads.views import main
+from ads.views import main, CategoryListCreateView, CategoryDetailView
 
 urlpatterns = [
     path('', main),
+    path('cat/', CategoryListCreateView.as_view()),
+    path('cat/<int:pk>/', CategoryDetailView.as_view()),
+
     path('admin/', admin.site.urls),
 ]
