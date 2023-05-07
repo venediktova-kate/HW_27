28db397defd8205e7de98dceee12f4728e5ad307
venediktova-kate <venediktova.kat@gmail.com>
2023-05-08 01:05:37 +0700

    Create 'category' views

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,33 @@
+import json
+
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
+
+from ads.models import Category
 
 
 def main(request):
     return JsonResponse({"status": "ok"})
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class CategoryListCreateView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+        return JsonResponse([{"id": cat.pk, "name": cat.name} for cat in categories], safe=False)
+
+    def post(self, request):
+        data = json.loads(request.body)
+        new = Category.objects.create(name=data.get("name"))
+        return JsonResponse({"id": new.pk, "name": new.name})
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+
+    def get(self, request, *args, **kwargs):
+        cat = self.get_object()
+        return JsonResponse({"id": cat.pk, "name": cat.name})
